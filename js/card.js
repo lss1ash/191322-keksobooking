af60ac9136c2c72edaf0837d701980857b406f3c
Alexander <lss1ash@rambler.ru>
2017-04-13 14:38:49 +1100

    Уменьшено количество вызовов querySelector()

--- js/card.js
+++ js/card.js
@@ -8,13 +8,8 @@
     bungalo: 'Бунгало'
   };
 
-  function setText(root, selector, text) {
-    root.querySelector(selector).textContent = text;
-  }
-
-  function fillFeatures(lodgeClone, item) {
-    var lodgeCloneFeatures = lodgeClone.querySelector('.lodge__features');
-    item.offer.features.forEach(function (feature) {
+  function fillFeatures(lodgeCloneFeatures, features) {
+    features.forEach(function (feature) {
       var newSpan = document.createElement('span');
       newSpan.classList.add('feature__image', 'feature__image--' + feature);
       lodgeCloneFeatures.appendChild(newSpan);
@@ -22,6 +17,7 @@
   }
 
   var offerDialog = document.getElementById('offer-dialog');
+  var offerAvatar = offerDialog.querySelector('.dialog__title').children[0];
   var currentDialog = offerDialog.querySelector('.dialog__panel');
   var dialogClose = offerDialog.querySelector('.dialog__close');
   var lodgeTemplate = document.getElementById('lodge-template');
@@ -39,18 +35,19 @@
     },
     fill: function (item) {
       var lodgeClone = lodgeTemplate.content.cloneNode(true);
-      setText(lodgeClone, '.lodge__title', item.offer.title);
-      setText(lodgeClone, '.lodge__address', item.offer.address);
-      lodgeClone.querySelector('.lodge__price').innerHTML = item.offer.price + '&#x20bd;/ночь';
-      setText(lodgeClone, '.lodge__type', OFFER_TYPE_DESCRIPTIONS[item.offer.type]);
-      setText(lodgeClone, '.lodge__rooms-and-guests', 'Для ' + item.offer.guests + ' гостей в ' + item.offer.rooms + ' комнатах');
-      setText(lodgeClone, '.lodge__checkin-time', 'Заезд после ' + item.offer.checkin + ', выезд до ' + item.offer.checkout);
-      fillFeatures(lodgeClone, item);
-      setText(lodgeClone, '.lodge__description', item.offer.description);
+      var cloneRoot = lodgeClone.children[0]; // dialog__panel
+      cloneRoot.children[0].textContent = item.offer.title;
+      cloneRoot.children[1].textContent = item.offer.address;
+      cloneRoot.children[2].innerHTML = item.offer.price + '&#x20bd;/ночь';
+      cloneRoot.children[3].textContent = OFFER_TYPE_DESCRIPTIONS[item.offer.type];
+      cloneRoot.children[4].textContent = 'Для ' + item.offer.guests + ' гостей в ' + item.offer.rooms + ' комнатах';
+      cloneRoot.children[5].textContent = 'Заезд после ' + item.offer.checkin + ', выезд до ' + item.offer.checkout;
+      fillFeatures(cloneRoot.children[6], item.offer.features);
+      cloneRoot.children[7].textContent = item.offer.description;
 
-      offerDialog.replaceChild(lodgeClone, currentDialog);
-      var offerAvatar = offerDialog.querySelector('.dialog__title').children[0];
       offerAvatar.setAttribute('src', item.author.avatar);
+      offerDialog.replaceChild(lodgeClone, currentDialog);
+
       currentDialog = offerDialog.querySelector('.dialog__panel');
     },
 
